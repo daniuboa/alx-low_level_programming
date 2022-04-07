5696399af600d0868424711c0a3c208f5bdce3c3
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-07 02:38:42 -0700

    Task 4: Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prnts the strng in the put function
+ *
+ * Description: Using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
