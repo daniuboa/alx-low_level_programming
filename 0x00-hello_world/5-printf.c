1dd39c3ebbb6874b439cb21d4adc5cae99744e10
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-07 03:25:19 -0700

    Task 5: Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
-* main - This function prints out the string
-* Description: using the main function, this program prints
+* main - This function prints the string
 * with proper grammar, but the outcome is a piece of art,
-* return - this function returns 0
+* Return: This function returns 0
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
