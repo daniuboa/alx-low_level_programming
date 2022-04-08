7b5cc1de510f229b2b7eb253bbe65625062f2090
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-08 03:39:30 -0700

    Task 1: Corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,6 @@ int main(void)
 	else if (lastNum == 10)
 		printf("Last digit of %d is %d and is 0\n", n, lastNum);
 	else
-		printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, lastNum);
+		printf("Last digit of %d is %d is less than 6 and not 0\n", n, lastNum);
 	return (0);
 }
