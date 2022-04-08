67660dafadd33a1d1f87549f5d9d4ccc483b377d
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-08 06:17:09 -0700

    Advanced Task 12: Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; 1++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (j > i)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i * 100 + j != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
