df57875b0448f65bccbf70845bb1946143cf688e
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-12 04:09:42 -0700

    Task 8: I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,10 +21,10 @@ void jack_bauer(void)
 			mins_remainder = minutes % 10;
 			hours_remainder = hours % 10;
 			_putchar(hours / 10 + '0');
-			_putchar(hours-remainder + '0');
+			_putchar(hours_remainder + '0');
 			_putchar(':');
 			_putchar(minutes /10 + '0');
-			_puchar(mins_remainder + '0');
+			_putchar(mins_remainder + '0');
 			minutes++;
 			_putchar('\n');
 		}
