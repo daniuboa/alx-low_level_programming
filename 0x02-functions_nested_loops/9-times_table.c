d023499656548a9e18fc6fe50248264b6b8efe03
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-12 04:33:40 -0700

    Task 9: Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
 * times_table - prints tables
 *
-* Return: 0
+* Return: void
 */
 
 void times_table(void)
@@ -12,25 +12,21 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
-			if ((c / 10) == 0)
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
 			{
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				if (b != 0)
-				{
-					_putchar(' ');
-					_putchar((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_purchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
 			}
 		}
 		_putchar('\n');
