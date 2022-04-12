b17525d9dc82643ee732892afee6a86491e9ceff
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-12 02:40:36 -0700

    Task 0: _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - print "_putchar"
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i = 0;
+	char name = "_putchar";
+
+	while (i < 9)
+	{
+		_putchar(name_of_school[i]);
+		i++;
+	}
+	_putchar('\n');
+	return(0);
+}
