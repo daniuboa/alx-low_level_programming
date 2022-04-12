caa1ff10e910869af4e75e0b4e91fbfde0e8d664
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-12 04:41:02 -0700

    Task 10: a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - sums two integers and 
+* return the results.
+*
+* @num1: integer one.
+* @num2: integer two.
+*
+* Return: sum
+*/
+
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
