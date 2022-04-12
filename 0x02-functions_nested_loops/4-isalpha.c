a176d8d04fccafbafab310da8281db04500c8cda
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-12 03:18:13 -0700

    TAsk 4: isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isalpha - Return 1 if c is a letter. lowercase or uppercase
+*
+* @c: The in to print
+* Return: 0
+*/
+
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
