5b515fc661bc4d2c90593919deb106c188a98cd9
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-14 02:46:25 -0700

    Task 2: Corrections

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * mul - multiply two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+
 int mul(int a, int b)
 {
 	int res = a * b;
