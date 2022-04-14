0f0efbbcbdb25de5bab00a5e3f249cdc29f2ded0
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-14 02:16:58 -0700

    Task 4: I believe in mumbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i, '0');
+		}
+	}
+
+	_putchar('\n');
+}
