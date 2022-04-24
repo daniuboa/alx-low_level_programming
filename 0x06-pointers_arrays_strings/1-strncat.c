53af83ee5a0d3fd7fb5e7b22e290c8d0937361dd
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-24 15:50:36 -0700

    Task 1: Corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,9 +3,9 @@
 #include "main.h"
 
 /**
- * _strncat - Function that concantenates two string
+ * _strncat - Function that concantenates two strings.
  * @dest: Pointer to the null-terminated byte string to append to.
- * @src: Pointer to the character array to coppy from.
+ * @src: Pointer to the character array to copy from.
  * @n: Maximum number of characters to copy.
  *
  * Return: Pointer to dest.
@@ -13,5 +13,5 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	return(strncat(dest, src, n));
+	return (strncat(dest, src, n));
 }
