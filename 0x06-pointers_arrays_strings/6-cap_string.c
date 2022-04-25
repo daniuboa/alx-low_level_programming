8bbd42cdb9e0c3a42e1324f9170fcbfea2ef34b1
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-24 17:08:00 -0700

    Task 6: Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,63 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - capitalizes all words of a string.
- * @str: The string to be capitalized
+ * isLower - Determines whether ASCII is lowercase
+ * @c: Character
  *
- * Return: A pointer to the changed string.
+ * Return: 1 if true, 0 if false
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - Determines whether ASCII is a delimeter
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimeter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimeter[i])
+			return (1);
+
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: Input string
+ *
+ * Return: String with capitalized words
  */
 
 char *cap_string(char *s)
 {
-	int i, j;
+	char *ptr = s;
+	int foundDelimit = 1;
 
-	char delimeters[] = " \t\n,;.!?\"(){}";
-	
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-			s[0] = s[0] - 32;
-		for (j = 0; delimeters[j] && s[i + 1] >= 97 && s[i +1] <= 122)
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
 		{
-			s[i + 1] = s[i + 1] - 32;
+			*s -= 32;
+			foundDelimit = 0;
 		}
+		else
+		{
+			foundDelimit = 0;
+		}
+		s++;
 	}
-
-	return (s);
+	return (ptr);
 }
