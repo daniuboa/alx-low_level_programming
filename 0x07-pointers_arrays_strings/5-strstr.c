bda843306d501e95ffc1c75276d52804ddeb20dd
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-27 10:40:15 -0700

    Task 5: strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,47 +2,29 @@
 #include <stdio.h>
 
 /**
- * compare - Function that compares two strings.
+ * _strstr - Finds the first occuerence of the substring in thestring haystack.
  *
- * @haystack: Pointer to the first null-terminated byte string
- * @needle: Pointer to the second null-terminated byte string.
+ * @haystack: Parameter for haystack
+ * @needle: Parameter for needle
  *
- * Return: True if haystack and needle are the same.
+ * Return: Pointer to the beginning of the located substring
  */
-
-int compare(char *haystack, char *needle)
-{
-	while (*haystack && *needle)
-	{
-		if (*haystack != *needle)
-			return (0);
-
-		haystack++;
-		needle++;
-	}
-
-	return (*needle == '\0');
-}
-
-/**
- * _strstr - Function that locates a substring.
- *
- * @haystack: Pointer to the null-terminated byte string to examine.
- * @needle: Pointer to the null-terminated byte string to be searched.
- *
- * Return: A pointer to the beginning of the located substring, or NULL
- */
-
 char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack != '\0')
 	{
-		if ((*haystack == *needle) && compare(haystack, needle))
+		char *beginning = haystack;
+		char *pattern = needle;
+
+		while (*pattern == *haystack && *pattern != '\0' && *haystack != '\0')
 		{
-			return (haystack);
+			haystack++;
+			pattern++;
 		}
 
-		haystack++;
+		if (*pattern == '\0')
+			return (beginning);
+		haystack = beginning + 1;
 	}
 
 	return ('\0');
