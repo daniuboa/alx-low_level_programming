af50204747ce2a8010e77261614eceadb1ba4b8e
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-27 07:42:35 -0700

    Task 3: Corrections

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,37 +1,37 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * _strspn - Function that gets length of a prefix string.
+ * _strspn - Gets the length of a prefix substring.
  *
- * @s: Pointer to the null-terminated byte string to be analyzed.
- * @accept: Pointer to the null-terminated byte string that contains characters.
+ * @s: The string to be served.
+ * @accept: The prefix to be measured.
  *
- * Return: Number of bytes in theinitial segment of s
+ * Return: The number of bytes in s which consist only of bytes fro accept.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int length, j;
+	unsigned int bytes = 0;
+	int index;
 
-	/* Loop through strings */
-	for (length = 0; *(s + length) != '\0'; length++)
+	while (*s)
 	{
-		/* Loop through tring accept */
-		for (j =  0; *(accept + j) != '\0'; j++)
+		for (index = 0; accept[index]; index++)
 		{
-			/* Check where the first character in string j appears */
-			/* in string c and immediately break out of the loop */
-			if (*(s + length) == *(accept + j))
+			if (*s == accept[index])
 			{
+				bytes++;
 				break;
 			}
+
+			else if (accept[index + 1] == '\0')
+			{
+				return (bytes);
+			}
 		}
-		/* Check if there is no string accept to loop through */
-		if (! *(accept + j))
-			break;
+
+		s++;
 	}
-	/* Return the length where the first character appears */
-	return (length);
+
+	return (bytes);
 }
