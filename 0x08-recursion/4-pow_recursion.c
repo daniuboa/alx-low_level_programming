5a894d885318fe3a8330028a567c2210f5f6a6de
Daniel Uboa <uboadaniel@gmail.com>
2022-04-28 12:04:17 +0300

    Task 4: nce an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - Function that returns the value of x raised to power y.
+ * @:x Parameter for the number
+ * @:y Number to be raised to.
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    {
+        return (-1);
+    }
+    else if (y == 0)
+    {
+        return (1);
+    }
+
+    return (x * _pow_recursion(x, y - 1));
+}
