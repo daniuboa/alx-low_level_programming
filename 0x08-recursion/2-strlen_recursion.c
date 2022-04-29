8bf28479f037728a1a3102d53a067dd1d27f1bb8
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-29 12:50:29 -0700

    Task 2: Corrections

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of the string.
- * @s: The strint to be measured.
- * 
- * Return: The length of the string.
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to bemeasured.
+ *
+ * Return: The length of string.
  */
 
 int _strlen_recursion(char *s)
 {
 	int len = 0;
+
 	if (*s)
 	{
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
+
 	return (len);
 }
