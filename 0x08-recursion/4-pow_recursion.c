f59b30bcef7dc91bad47bd6807ad7b4eacb826a9
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-04-29 13:03:26 -0700

    Task 4: Corrections

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,20 +3,19 @@
 
 /**
  * _pow_recursion - Function that returns the value of x raised to power y.
- * @:x Parameter for the number
- * @:y Number to be raised to.
+ * @x: Parameter for the number
+ * @y: Number to be raised to.
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return (-1);
-    }
-    else if (y == 0)
-    {
-        return (1);
-    }
-
-    return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
