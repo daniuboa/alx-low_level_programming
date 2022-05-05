991a04ae1c46d425292d3659490c933e98373ab4
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-05 07:40:40 -0700

    Advanced Task 5: Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,6 +8,15 @@
  * @argc: number of arguments passed to the function.
  * @argv: argument vector of pointers to strings.
  *
+ * Usage: ./change cents where cents is the amount of you need to give back.
+ * if the number of arguments passed is not exactly 1,
+ * print Error, followed by a new line, and return 1.
+ * you should use atoi to parse the parameter passed to your program
+ * If the number passed as the argument is negative, print 0,
+ * followed by a new line.
+ * You can use an unlimited number of coints of values 25, 10, 5, 2,
+ * and 1 cent.
+ *
  * Return: 0 (Success)
  */
 
