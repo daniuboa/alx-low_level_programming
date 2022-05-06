0bdcbc16303f61a2c6fa5c86041dbb3572b97ea4
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-06 05:20:38 -0700

    Task 0: Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ *
+ * @size: argument for size of the array
+ * @c: argument for char
+ *
+ * Return: 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *my_array;
+	unsigned int i = 0;
+
+	my_array = malloc(size * sizeof(c));
+
+	while (i < size)
+	{
+		if (my_array == NULL)
+		{
+			return (NULL);
+		}
+
+		my_array[i] = c;
+		i++;
+	}
+
+	return (my_array);
+}
