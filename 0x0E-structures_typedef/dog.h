152c6c90d31e68cb59c360878bae733f969feb5d
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-12 01:10:50 -0700

    Task 0: Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,36 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * File: dog.h
+ *
+ * Desc: Header file that defines a nw type struct dog.
+ */
+
+/**
+ * struct dog - Anew type describing a dog.
+ *
+ * @name: The name of the dog.
+ * @age: Theage of the dog.
+ * @owner: The owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
