7be3bc301c00cff0a586ad4957449e19e90a5bc8
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-16 15:11:13 -0700

    Task 2: To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer
+ *
+ * @array: An array of integers
+ * @size: Number of elements on the @array
+ * @cmp: Pointer to the function to be used to compare values
+ *
+ * Return: Integers
+ */
+
+int int_index(int *array, size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || !array || !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
