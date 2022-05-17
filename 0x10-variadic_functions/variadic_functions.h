1a42a3c5bcf397d568b705c7639381113a2e6d51
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-17 11:42:49 -0700

    Task 0: Corrections

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,13 +4,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * struct format_struc - Format-type class
- *
- * @format: Format identifier
- * @fmt_print_funct: Pointers to function that prints in the format of @format
- */
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
