698028b22192145feb5ca8b98d1c8e077b4d76d5
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-17 01:14:54 -0700

    Task 0: Beauty is variable, ugliness is constant

---
+++ 0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - Sum of all its parameters
+ *
+ * @n: Total number of parameters
+ *
+ * Return: SUm of value parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
+	if (n ==0)
+		return (sum);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
