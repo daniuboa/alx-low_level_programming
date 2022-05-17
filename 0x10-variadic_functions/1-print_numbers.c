0d689710d819c07edafcb8937f5e3c73affcd909
Ukech Daniel Uboa <uboadaniel@gmail.com>
2022-05-17 02:46:09 -0700

    Task 1: Corrections

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(params, n);
 
-	for (i = 0; i < (n -1) && n != 0; i++)
+	for (i = 0; i < (n - 1) && n != 0; i++)
 	{
 		if (!separator)
 			printf("%d", va_arg(params, int));
