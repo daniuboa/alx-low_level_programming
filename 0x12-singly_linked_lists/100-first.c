1a40c59c320df9bcee617c2a73738ee84aef1cf1
Daniel Uboa <uboadaniel@gmail.com>
2022-05-24 04:59:29 -0700

    Create 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void __attribute__((constructor)) hare(void);
  * main function is executed.
  */
 
-void hare (void)
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
