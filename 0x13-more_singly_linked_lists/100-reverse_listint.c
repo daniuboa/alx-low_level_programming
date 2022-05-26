417e413d5b2415acd5450cd46aef01c565310e36
Daniel Uboa <uboadaniel@gmail.com>
2022-05-26 05:18:34 -0700

    100-reverse_listint.c crrections

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,7 +9,7 @@
  * Return: A pointer to the first node of the reversed list.
  */
 
-listint_t *reversed_listint(listint_t **head)
+listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *ahead, *behind;
 
