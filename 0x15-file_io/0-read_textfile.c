76d359e814d457d4a8af7d27531f1d1f8f0c3002
Daniel Uboa <uboadaniel@gmail.com>
2022-06-01 23:28:03 -0700

    Create 0-main.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	R = read(o, buffer, letters);
+	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
